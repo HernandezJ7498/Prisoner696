d76a0b6236167e139694eefc778a7d317107c3c7
jch38 <jch38@njit.edu>
2018-10-05 11:54:36 -0400

    Initial unity commit

---
+++ Assets/Scripts/GunFire.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunFire : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+			AudioSource gunsound = gameObject.GetComponent<AudioSource> ();
+			gunsound.Play ();
+			gameObject.GetComponent<Animation> ().Play ("GunShot");
+	}
+}
+}
