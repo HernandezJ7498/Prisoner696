44cf55ba468b65fc98ad8d8aa5e259e527389941
Juan Hernandez <jch38@njit.edu>
2018-10-22 19:16:35 -0400

    added new enemy and animation, player health, damage mechanism

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class EnemyMove : MonoBehaviour {
 
 	// Use this for initialization
-	GameObject
+	public GameObject ThePlayer;
+	public GameObject TheEnemy;
+	public float EnemySpeed;
+	public int MoveTrigger;
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (MoveTrigger == 0) {
+			EnemySpeed = 0.02f;
+			transform.position = Vector3.MoveTowards (transform.position, ThePlayer.transform.position, EnemySpeed);
+		}
 	}
 }
---
+++ Assets/Scripts/GlobalHealth.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GlobalHealth : MonoBehaviour {
+
+	// Use this for initialization
+	public static int PlayerHealth = 10;
+	public int InternalHealth;
+	public GameObject HealthDisplay;
+	void Start(){
+
+	}
+	void Update(){
+		InternalHealth = PlayerHealth;
+		HealthDisplay.GetComponent<Text> ().text = "Health: " + PlayerHealth;
+		if (PlayerHealth == 0) {
+			//SceneManager.LoadScene (1);
+		}
+	}
+}
