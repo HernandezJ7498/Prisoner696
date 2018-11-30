3063c238b729c36ed8fb385aa1027c797d98163b
Juan Hernandez <jch38@njit.edu>
2018-11-29 23:28:26 -0500

    all offices interior done, added paper to office 1 and collision to desk of office 2

---
+++ Assets/BoltNotice.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoltNotice : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject SwitchAlert;
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerStay(){
+		SwitchAlert.GetComponent<Text> ().text = "Click on the Bolt to turn it";
+	}
+	void OnTriggerExit(){
+		SwitchAlert.GetComponent<Text> ().text = "";
+	}
+}
