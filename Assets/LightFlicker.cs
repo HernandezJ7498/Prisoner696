a359a82eb2e04e6d9b5a1983c653666237f32bae
Juan Hernandez <jch38@njit.edu>
2018-12-02 02:27:48 -0500

    finished power room and cafeteria ceiling started control room assets

--- Assets/LightFlicker.cs
+++ Assets/LightFlicker.cs
@@ -8,16 +8,21 @@ public class LightFlicker : MonoBehaviour {
 	public GameObject SpotLight;
 	public GameObject AreaLight;
 	float number;
+	bool ExecuteOnce;
 	void Start () {
 		StartCoroutine (LightFlickerRoutine());
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(GameManager.instance.ElevatorPower && !ExecuteOnce){
+			SpotLight.GetComponent<Light>().enabled = !SpotLight.GetComponent<Light>().enabled;
+			AreaLight.GetComponent<Light>().enabled = !AreaLight.GetComponent<Light>().enabled;
+			ExecuteOnce = true;
+		}
 	}
 	IEnumerator LightFlickerRoutine(){
-		while(true){
+		while(!GameManager.instance.ElevatorPower){
 			number = Random.Range (0.0f, .9f);
 			yield return new WaitForSeconds(number);
 			SpotLight.GetComponent<Light>().enabled = !SpotLight.GetComponent<Light>().enabled;
