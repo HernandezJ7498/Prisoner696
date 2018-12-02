dcd998815fa291dd9f9b5056a7c583c36e08104a
Juan Hernandez <jch38@njit.edu>
2018-12-01 21:08:31 -0500

    added blinking smoke alarm and finishined texturing and lighting in main area. added assests to caffeteria includding counter, tables and chairs. some lighting. door to back room is done without collision

---
+++ Assets/LightBlink.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightBlink : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject redlight;
+	void Start () {
+		StartCoroutine (Blink());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	IEnumerator Blink(){
+		while(true){	
+			yield return new WaitForSeconds (1.5f);
+			redlight.GetComponent<Light> ().enabled = !redlight.GetComponent<Light> ().enabled;
+		}
+	}
+
+}
---
+++ Assets/LightFlicker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject SpotLight;
+	public GameObject AreaLight;
+	float number;
+	void Start () {
+		StartCoroutine (LightFlickerRoutine());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	IEnumerator LightFlickerRoutine(){
+		while(true){
+			number = Random.Range (0.0f, .9f);
+			yield return new WaitForSeconds(number);
+			SpotLight.GetComponent<Light>().enabled = !SpotLight.GetComponent<Light>().enabled;
+			AreaLight.GetComponent<Light>().enabled = !AreaLight.GetComponent<Light>().enabled;
+		}
+
+		
+	}
+}
