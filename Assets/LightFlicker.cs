c5a139ac64091f894d92277bb73a8fe70a3e6c46
Juan Hernandez <jch38@njit.edu>
2018-12-03 14:26:42 -0500

    added computers and cables to long office new door for rocket room and cables to open it

--- Assets/LightFlicker.cs
+++ Assets/LightFlicker.cs
@@ -8,25 +8,35 @@ public class LightFlicker : MonoBehaviour {
 	public GameObject SpotLight;
 	public GameObject AreaLight;
 	float number;
-	bool ExecuteOnce;
+	public int LightColor;
+	public Material Yellow;
+	public Material DarkYellow;
+	public GameObject Lamp;
 	void Start () {
 		StartCoroutine (LightFlickerRoutine());
+		LightColor = 1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(GameManager.instance.ElevatorPower && !ExecuteOnce){
+		/*if(GameManager.instance.ElevatorPower && !ExecuteOnce){
 			SpotLight.GetComponent<Light>().enabled = !SpotLight.GetComponent<Light>().enabled;
 			AreaLight.GetComponent<Light>().enabled = !AreaLight.GetComponent<Light>().enabled;
 			ExecuteOnce = true;
-		}
+		}*/
 	}
 	IEnumerator LightFlickerRoutine(){
 		while(!GameManager.instance.ElevatorPower){
 			number = Random.Range (0.0f, .9f);
+			if (LightColor % 2 == 1) {
+				Lamp.GetComponent<MeshRenderer> ().material = Yellow;
+			} else {
+				Lamp.GetComponent<MeshRenderer> ().material = DarkYellow;
+			}
 			yield return new WaitForSeconds(number);
 			SpotLight.GetComponent<Light>().enabled = !SpotLight.GetComponent<Light>().enabled;
 			AreaLight.GetComponent<Light>().enabled = !AreaLight.GetComponent<Light>().enabled;
+			LightColor++;
 		}
 
 		
